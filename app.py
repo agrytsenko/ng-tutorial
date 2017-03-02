00935d1a6222f43f8812fb0c7092cb20ad00c011
Alexander Grytsenko <alexander.grytsenko@agent.ai>
2017-03-02 22:34:08 +0200

    simplified routing for flask app

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
 from __future__ import unicode_literals
 
+import os
+
 from flask import Flask
-from flask import request, render_template, send_from_directory
+from flask import request, send_from_directory
 
 
 app = Flask(__name__, static_url_path='')
 route = app.route
 
 
-@route('/favicon.ico')
-def favicon():
-    return send_from_directory('templates', 'favicon.ico')
-
-
-@route('/js/<path:path>')
-def serve_static(path):
-    return send_from_directory('js', path)
-
-
 @route('/')
-def init():
-    return send_from_directory('templates', 'home.html')
+def index():
+    return send_from_directory('templates', 'index.html')
 
+
+shared_files = {
+    '/js': os.path.join(os.path.dirname(__file__), 'js'),
+    '/': os.path.join(os.path.dirname(__file__), 'templates')
+
+}
 
 if __name__ == '__main__':
-    app.run('localhost', 5000, debug=True)
+    app.run(
+        host='localhost',
+        port=5000,
+        debug=True,
+        static_files=shared_files
+    )
 
 
 
