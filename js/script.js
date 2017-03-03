0b8ab52cc63a103192be8ad9c881ee7be6d93bce
Alexander Grytsenko <alexander.grytsenko@agent.ai>
2017-03-03 21:50:55 +0200

    serach by a table columns

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ angular
     .controller("myController", function($scope) {
 
         var employees = [
-            {name: 'Ben', dateOfBirth: new Date('October 6, 1984'), gender: 'Male', salary: 500.125},
-            {name: 'Alex', dateOfBirth: new Date('November 16, 1980'), gender: 'Male', salary: 123123.23423},
-            {name: 'Peter', dateOfBirth: new Date('Jan 24, 1945'), gender: 'Male', salary: 23434545.2323},
-            {name: 'Alan', dateOfBirth: new Date('Feb 5, 1987'), gender: 'Male', salary: 4545.5656},
-            {name: 'Grethen', dateOfBirth: new Date('March 18, 1978'), gender: 'Female', salary: 123234.567},
-            {name: 'Inga', dateOfBirth: new Date('Jun 29, 1934'), gender: 'Female', salary: 12321355.1},
-            {name: 'Mary', dateOfBirth: new Date('Sep 23, 1977'), gender: 'Female', salary: 500},
-            {name: 'Brenda', dateOfBirth: new Date('Nov 11, 1988'), gender: 'Female', salary: 123456.43},
-            {name: 'Vadim', dateOfBirth: new Date('Dec 1, 1998'), gender: 'Male', salary: 4343.6565}
+            {name: 'Ben', gender: 'Male', salary: 500.125, city: 'London'},
+            {name: 'Alex', gender: 'Male', salary: 123123.23423, city: 'Vancouver'},
+            {name: 'Peter', gender: 'Male', salary: 23434545.2323, city: 'New-York'},
+            {name: 'Alan', gender: 'Male', salary: 4545.5656, city: 'London'},
+            {name: 'Grethen', gender: 'Female', salary: 123234.567, city: 'Vancouver'},
+            {name: 'Inga', gender: 'Female', salary: 12321355.1, city: 'Tokyo'},
+            {name: 'Mary', gender: 'Female', salary: 500, city: 'Vancouver'},
+            {name: 'Brenda', gender: 'Female', salary: 123456.43, city: 'New-York'},
+            {name: 'Vadim', gender: 'Male', salary: 4343.6565, city: 'London'}
         ];
 
         $scope.employees = employees;
