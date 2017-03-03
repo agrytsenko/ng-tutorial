39d851503aba7d7e19fb46390e89d36b39632231
Alexander Grytsenko <alexander.grytsenko@agent.ai>
2017-03-03 21:28:15 +0200

    columns sorting order

--- js/script.js
+++ js/script.js
@@ -17,5 +17,18 @@ angular
         ];
 
         $scope.employees = employees;
-        $scope.columnOrder = 'name';
+        $scope.sortColumn = null;
+        $scope.reverseSort = false;
+
+        $scope.sortData = function(column) {
+            $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+            $scope.sortColumn = column;
+        };
+
+        $scope.getSortClass = function(column) {
+            if ($scope.sortColumn == column) {
+                return $scope.reverseSort ? 'arrow-down' : 'arrow-up'
+            }
+            return ''
+        };
 });
