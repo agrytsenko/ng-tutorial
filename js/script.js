ade69b1d565dd25e093e61064384e8c9ac3eb987
Alexander Grytsenko <alexander.grytsenko@agent.ai>
2017-03-04 19:14:14 +0200

    ng-include, list/table views

--- js/script.js
+++ js/script.js
@@ -4,35 +4,37 @@ var app = angular
     .module("myModule", [])
     .controller("myController", function($scope) {
 
-        // var employees = [
-        //     {name: 'Ben', gender: 'Male', salary: 500.125, city: 'London'},
-        //     {name: 'Alex', gender: 'Male', salary: 3123.23423, city: 'Vancouver'},
-        //     {name: 'Peter', gender: 'Male', salary: 34545.2323, city: 'New-York'},
-        //     {name: 'Alan', gender: 'Male', salary: 4545.5656, city: 'London'},
-        //     {name: 'Grethen', gender: 'Female', salary: 3234.567, city: 'Vancouver'},
-        //     {name: 'Inga', gender: 'Female', salary: 355.1, city: 'Tokyo'},
-        //     {name: 'Mary', gender: 'Female', salary: 500, city: 'Vancouver'},
-        //     {name: 'Brenda', gender: 'Female', salary: 123456.43, city: 'New-York'},
-        //     {name: 'Vadim', gender: 'Male', salary: 4343.6565, city: 'London'}
-        // ];
-
-        var countries = [
-            {
-                name: 'Canada',
-                cities: [
-                    {name: 'Vancouver'},
-                    {name: 'Toronto'}
-                ]
-            },
-            {
-                name: 'USA',
-                cities: [
-                    {name: 'Chicago'},
-                    {name: 'New York'}
-                ]
-            }
+        var employees = [
+            {name: 'Ben', gender: 'Male', salary: 500.125, city: 'London'},
+            {name: 'Alex', gender: 'Male', salary: 3123.23423, city: 'Vancouver'},
+            {name: 'Peter', gender: 'Male', salary: 34545.2323, city: 'New-York'},
+            {name: 'Alan', gender: 'Male', salary: 4545.5656, city: 'London'},
+            {name: 'Grethen', gender: 'Female', salary: 3234.567, city: 'Vancouver'},
+            {name: 'Inga', gender: 'Female', salary: 355.1, city: 'Tokyo'},
+            {name: 'Mary', gender: 'Female', salary: 500, city: 'Vancouver'},
+            {name: 'Brenda', gender: 'Female', salary: 123456.43, city: 'New-York'},
+            {name: 'Vadim', gender: 'Male', salary: 4343.6565, city: 'London'}
         ];
 
-        $scope.countries = countries;
+        $scope.employees = employees;
+        $scope.employeeView = 'employeeTable.html';
 
 });
+
+
+        // var countries = [
+        //     {
+        //         name: 'Canada',
+        //         cities: [
+        //             {name: 'Vancouver'},
+        //             {name: 'Toronto'}
+        //         ]
+        //     },
+        //     {
+        //         name: 'USA',
+        //         cities: [
+        //             {name: 'Chicago'},
+        //             {name: 'New York'}
+        //         ]
+        //     }
+        // ];
