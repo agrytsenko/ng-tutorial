db7c71e073b20d976bb3f23ab09fc62148546f0e
Alexander Grytsenko <alexander.grytsenko@agent.ai>
2017-03-04 18:55:20 +0200

    ng-init

--- js/script.js
+++ js/script.js
@@ -2,21 +2,37 @@
 
 var app = angular
     .module("myModule", [])
-    .filter()
     .controller("myController", function($scope) {
 
-        var employees = [
-            {name: 'Ben', gender: 'Male', salary: 500.125, city: 'London'},
-            {name: 'Alex', gender: 'Male', salary: 3123.23423, city: 'Vancouver'},
-            {name: 'Peter', gender: 'Male', salary: 34545.2323, city: 'New-York'},
-            {name: 'Alan', gender: 'Male', salary: 4545.5656, city: 'London'},
-            {name: 'Grethen', gender: 'Female', salary: 3234.567, city: 'Vancouver'},
-            {name: 'Inga', gender: 'Female', salary: 355.1, city: 'Tokyo'},
-            {name: 'Mary', gender: 'Female', salary: 500, city: 'Vancouver'},
-            {name: 'Brenda', gender: 'Female', salary: 123456.43, city: 'New-York'},
-            {name: 'Vadim', gender: 'Male', salary: 4343.6565, city: 'London'}
+        // var employees = [
+        //     {name: 'Ben', gender: 'Male', salary: 500.125, city: 'London'},
+        //     {name: 'Alex', gender: 'Male', salary: 3123.23423, city: 'Vancouver'},
+        //     {name: 'Peter', gender: 'Male', salary: 34545.2323, city: 'New-York'},
+        //     {name: 'Alan', gender: 'Male', salary: 4545.5656, city: 'London'},
+        //     {name: 'Grethen', gender: 'Female', salary: 3234.567, city: 'Vancouver'},
+        //     {name: 'Inga', gender: 'Female', salary: 355.1, city: 'Tokyo'},
+        //     {name: 'Mary', gender: 'Female', salary: 500, city: 'Vancouver'},
+        //     {name: 'Brenda', gender: 'Female', salary: 123456.43, city: 'New-York'},
+        //     {name: 'Vadim', gender: 'Male', salary: 4343.6565, city: 'London'}
+        // ];
+
+        var countries = [
+            {
+                name: 'Canada',
+                cities: [
+                    {name: 'Vancouver'},
+                    {name: 'Toronto'}
+                ]
+            },
+            {
+                name: 'USA',
+                cities: [
+                    {name: 'Chicago'},
+                    {name: 'New York'}
+                ]
+            }
         ];
 
-        $scope.employees = employees;
+        $scope.countries = countries;
 
 });
