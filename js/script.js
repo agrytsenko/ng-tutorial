a47fa31ce0015be57513e6cf6c4b597c0fc9ed7e
Alexander Grytsenko <alexander.grytsenko@agent.ai>
2017-03-15 13:57:49 +0200

    routing (part 1)

--- js/script.js
+++ js/script.js
@@ -3,13 +3,5 @@
 var app = angular
     .module("demoApp", [])
     .controller("myController", function($scope, $http, $location, $anchorScroll) {
-        $http.get('country/')
-             .then(function (response) {
-                 $scope.countries = response.data;
-             });
 
-        $scope.scrollTo = function (countryName) {
-            $location.hash(countryName);
-            $anchorScroll();
-        };
 });
